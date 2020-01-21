dbf224fe0ca6a0db871f4959cb373c0ae6d9ae76
yamato3310 <yamato3310.tuna@gmail.com>
2020-01-21 17:00:39 +0900

    builderパターン

--- src/Main.scala
+++ src/Main.scala
@@ -1,65 +1,59 @@
 /*
-今回の目的は、予算範囲内で商品を買うか判断する戦略と特定の種類の商品だけを買うか判断する戦略
-商品には3種類("beauty", "food", "toy")あり、購入者はそれぞれの種類に対して一つの商品で予算の何パーセントのまでの金額を出せるかを決めている
-商品を購入した際は購入した商品と残りの残高を表示する
-- 予算範囲内で商品を買うか判断する戦略: 予算内で上記の条件に従って商品を購入する。予算が足りないときは購入しない
-- 特定の種類の商品だけを買うか判断する戦略: 指定した種類の商品だけを購入するが、予算が足りない場合は万引きする
+今回のサンプルコードは、レジシステムです
+ShoppingBasketでbuilderパターンを使用しています
+このクラスに購入する商品を入れていき、receiptBuild関数を使用することでレシートを作っています
+
+レジに商品を入れることで、会計をしてレシートを出力する
+
 */
 
 import scala.math._
 
 object Main extends App {
-  val user = new User(10000, 10000, 0.2, 0.5, 0.2)
-  val products = new ProductRepository().create(20)
-  val shop = Shop(products)
-  val strategy = new UnlimitedGenreStrategy()
-  user.shopping(shop, strategy)
+  val products = new ProductRepository().create(10)
+  val user = User("nabeshi", 10000, products)
+  val register = new Register(new ReceiptBuilder())
+  val receipt = register.addProducts(user.shoppingBasket).takeProduct(user.shoppingBasket(1)).accounting(user.money)
+  receipt.print()
+}
+
+class Register(builder: ReceiptBuilder) {
+  def addProducts(products: Seq[Product]): Register= {
+    new Register(products.foldLeft(builder)((builder, product) => builder.addProduct(product)))
+  }
+  def takeProduct(product: Product): Register = {
+    new Register(builder.takeProduct(product))
+  }
+  def accounting(money: Int): Receipt = builder.build(money)
 }
 
 class ProductRepository() {
-  private val genres = List("beauty", "food", "toy")
-
   def create(num: Int): List[Product] = for {
     i <- (1 to num).toList
   } yield {
-    val genre = genres(floor(random() * genres.length).toInt)
-    Product(i, s"$genre$i", floor(random() * 5000).toInt, genre)
+    Product(s"product$i", floor(random() * 1000).toInt)
   }
 }
 
-case class Product(id: Int, name: String, price: Int, genre: String)
+case class Product(name: String, price: Int)
 
-class User(private var wallet: Int, val budget: Int, val maxBeautyPercentage: Double, val maxFoodPercentage: Double, val maxToyPercentage: Double) {
-  private def buy(product: Product): Unit = {
-    setWallet(product.price)
-    println(s"${product.price}円の${product.name} を購入した！　残り$wallet 円")
-  }
-
-  private def notBuy(product: Product): Unit = println(s"${product.price}円の${product.name}を購入しませんでした")
-
-  private def setWallet(price: Int): Unit = wallet -= price
-
-  def getWallet(): Int = wallet
-
-  def shopping(shop: Shop, strategy: ShoppingStrategy): Unit =
-    shop.products.foreach(product => if (strategy.judgeBuy(this, product)) buy(product) else notBuy(product))
-}
-
-case class Shop(products: List[Product])
-
-trait ShoppingStrategy {
-  def judgeBuy(user: User, product: Product): Boolean
-}
-
-class UnlimitedGenreStrategy() extends ShoppingStrategy {
-  override def judgeBuy(user: User, product: Product): Boolean = {
-    val budgetPercentage = product.price.toDouble / user.budget
-    println(budgetPercentage)
-    product.genre match {
-      case _ if user.getWallet() < product.price => false
-      case "beauty" => budgetPercentage <= user.maxBeautyPercentage
-      case "food" => budgetPercentage <= user.maxFoodPercentage
-      case "toy" => budgetPercentage <= user.maxToyPercentage
-    }
+case class Receipt(products: Seq[Product], payment: Int) {
+  val totalFee = products.map(_.price).sum
+  def print(): Unit = {
+    products.foreach(product => println(s"${product.name}: ${product.price} 円"))
+    println(s"合計: $totalFee 円")
+    println(s"お預かり: $payment 円")
+    println(s"お釣り: ${payment - totalFee} 円")
   }
 }
+
+class ReceiptBuilder(private val productList: Seq[Product] = Seq()) {
+  // レジを通した商品を追加していく
+  def addProduct(product: Product): ReceiptBuilder = new ReceiptBuilder(productList :+ product)
+  // キャンセルした商品を取り出す
+  def takeProduct(product: Product): ReceiptBuilder = new ReceiptBuilder(productList.diff(Seq(product)))
+  // レシートを生成する
+  def build(money: Int): Receipt = Receipt(productList, money)
+}
+
+case class User(name: String, money: Int, shoppingBasket: Seq[Product])
