7da7b4904418088cefcee81f9a8ff6fb99d97fdb
yamato3310 <yamato3310.tuna@gmail.com>
2020-01-31 17:09:43 +0900

    修正

--- src/Main.scala
+++ src/Main.scala
@@ -3,7 +3,7 @@
 ShoppingBasketでbuilderパターンを使用しています
 このクラスに購入する商品を入れていき、receiptBuild関数を使用することでレシートを作っています
 
-レジに商品を入れることで、会計をしてレシートを出力する
+購入する商品をレジに登録し、会計をしてレシートを発行する。
 
 */
 
@@ -13,47 +13,56 @@ object Main extends App {
   val products = new ProductRepository().create(10)
   val user = User("nabeshi", 10000, products)
   val register = new Register(new ReceiptBuilder())
-  val receipt = register.addProducts(user.shoppingBasket).takeProduct(user.shoppingBasket(1)).accounting(user.money)
+  val receipt = register
+    .addProducts(user.shoppingBasket)
+    .cancelProduct(user.shoppingBasket(1))
+    .calculate(user.money)
   receipt.print()
 }
 
+// ReceiptBuilderを操作するクラス。ユーザーが商品を買う時はこのクラスを使用する
 class Register(builder: ReceiptBuilder) {
+  // 商品をbuilderに追加する
   def addProducts(products: Seq[Product]): Register= {
     new Register(products.foldLeft(builder)((builder, product) => builder.addProduct(product)))
   }
-  def takeProduct(product: Product): Register = {
-    new Register(builder.takeProduct(product))
+  // 商品をキャンセルする
+  def cancelProduct(product: Product): Register = {
+    new Register(builder.cancelProduct(product))
   }
-  def accounting(money: Int): Receipt = builder.build(money)
+  // 計算をしてレシートを出力する
+  def calculate(money: Int): Receipt = builder.build(money)
 }
 
+//　商品を取り出すためのクラス
 class ProductRepository() {
-  def create(num: Int): List[Product] = for {
-    i <- (1 to num).toList
-  } yield {
-    Product(s"product$i", floor(random() * 1000).toInt)
-  }
+  // 商品を取り出す
+  def create(num: Int): List[Product] = (1 to num).toList.map(i => Product(s"product$i", floor(random() * 1000).toInt))
 }
 
+// 商品
 case class Product(name: String, price: Int)
 
-case class Receipt(products: Seq[Product], payment: Int) {
-  val totalFee = products.map(_.price).sum
+// 購入した商品をまとめるクラス
+case class Receipt(products: Seq[Product], payment: Int, price: Int) {
+  // レシートを表示する
   def print(): Unit = {
     products.foreach(product => println(s"${product.name}: ${product.price} 円"))
-    println(s"合計: $totalFee 円")
+    println(s"合計: $price 円")
     println(s"お預かり: $payment 円")
-    println(s"お釣り: ${payment - totalFee} 円")
+    println(s"お釣り: ${payment - price} 円")
   }
 }
 
+// builderクラス。商品を追加していきbuild関数が呼び出されたタイミングで、レシートを生成している
 class ReceiptBuilder(private val productList: Seq[Product] = Seq()) {
   // レジを通した商品を追加していく
   def addProduct(product: Product): ReceiptBuilder = new ReceiptBuilder(productList :+ product)
-  // キャンセルした商品を取り出す
-  def takeProduct(product: Product): ReceiptBuilder = new ReceiptBuilder(productList.diff(Seq(product)))
+  // 商品をキャンセルする
+  def cancelProduct(product: Product): ReceiptBuilder = new ReceiptBuilder(productList.diff(Seq(product)))
   // レシートを生成する
-  def build(money: Int): Receipt = Receipt(productList, money)
+  def build(money: Int): Receipt = Receipt(productList, money, productList.map(_.price).sum)
 }
 
+// 商品を買うユーザー
 case class User(name: String, money: Int, shoppingBasket: Seq[Product])
