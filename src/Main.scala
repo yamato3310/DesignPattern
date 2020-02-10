6bcbc2cf73d19e7a0d5bf09539f9d56347c2e150
yamato3310 <yamato3310.tuna@gmail.com>
2020-02-10 15:53:21 +0900

    FactoryMethodパターン

--- src/Main.scala
+++ src/Main.scala
@@ -1,68 +1,60 @@
 /*
-今回のサンプルコードは、レジシステムです
-ShoppingBasketでbuilderパターンを使用しています
-このクラスに購入する商品を入れていき、receiptBuild関数を使用することでレシートを作っています
-
-購入する商品をレジに登録し、会計をしてレシートを発行する。
 
 */
 
 import scala.math._
 
 object Main extends App {
-  val products = new ProductRepository().create(10)
-  val user = User("nabeshi", 10000, products)
-  val register = new Register(new ReceiptBuilder())
-  val receipt = register
-    .addProducts(user.shoppingBasket)
-    .cancelProduct(user.shoppingBasket(1))
-    .calculate(user.money)
-  receipt.print()
+  val italianRestaurantFactory: ItalianRestaurantFactory = new ItalianRestaurantFactory
+  val italianRestaurant: ItalianRestaurant = italianRestaurantFactory.create(10)
+  italianRestaurant.printMenu()
+
+  val familyRestaurantFactory: WesternFoodRestaurantFactory = new WesternFoodRestaurantFactory
+  val familyRestaurant: WesternFoodRestaurant = familyRestaurantFactory.create(15)
+  familyRestaurant.printMenu()
 }
 
-// ReceiptBuilderを操作するクラス。ユーザーが商品を買う時はこのクラスを使用する
-class Register(builder: ReceiptBuilder) {
-  // 商品をbuilderに追加する
-  def addProducts(products: Seq[Product]): Register= {
-    new Register(products.foldLeft(builder)((builder, product) => builder.addProduct(product)))
-  }
-  // 商品をキャンセルする
-  def cancelProduct(product: Product): Register = {
-    new Register(builder.cancelProduct(product))
-  }
-  // 計算をしてレシートを出力する
-  def calculate(money: Int): Receipt = builder.build(money)
+// レストランの抽象クラス。今回はメニューを表示するだけ
+trait Restaurant {
+  val menu: Seq[Menu]
+  def printMenu(): Unit = println(menu)
 }
 
-//　商品を取り出すためのクラス
-class ProductRepository() {
-  // 商品を取り出す
-  def create(num: Int): List[Product] = (1 to num).toList.map(i => Product(s"product$i", floor(random() * 1000).toInt))
+// イタリアンレストラン
+class ItalianRestaurant(val menu: Seq[Menu]) extends Restaurant
+
+// 洋食レストラン
+class WesternFoodRestaurant(val menu: Seq[Menu]) extends Restaurant
+
+// レストランのメニュー
+case class Menu(name: String, price: Int)
+
+// レストランのFactoryクラス
+trait RestaurantFactory {
+  // この関数を呼ぶことで、レストランを作成することができる
+  def create(MenuNum: Int): Restaurant
 }
 
-// 商品
-case class Product(name: String, price: Int)
-
-// 購入した商品をまとめるクラス
-case class Receipt(products: Seq[Product], payment: Int, price: Int) {
-  // レシートを表示する
-  def print(): Unit = {
-    products.foreach(product => println(s"${product.name}: ${product.price} 円"))
-    println(s"合計: $price 円")
-    println(s"お預かり: $payment 円")
-    println(s"お釣り: ${payment - price} 円")
+// イタリアンレストランの抽象クラス
+class ItalianRestaurantFactory() extends RestaurantFactory {
+  // イタリアンレストランを作成するための初期処理を行っている
+  override def create(menuNum: Int): ItalianRestaurant = {
+    val menuSuffix = Seq("Pasta", "Pizza", "Soup")
+    val menu = (0 to menuNum).map(i => {
+      Menu(s"Italian${menuSuffix(i % 3)}$i", (random() * 2000).toInt)
+    })
+    new ItalianRestaurant(menu)
   }
 }
 
-// builderクラス。商品を追加していきbuild関数が呼び出されたタイミングで、レシートを生成している
-class ReceiptBuilder(private val productList: Seq[Product] = Seq()) {
-  // レジを通した商品を追加していく
-  def addProduct(product: Product): ReceiptBuilder = new ReceiptBuilder(productList :+ product)
-  // 商品をキャンセルする
-  def cancelProduct(product: Product): ReceiptBuilder = new ReceiptBuilder(productList.diff(Seq(product)))
-  // レシートを生成する
-  def build(money: Int): Receipt = Receipt(productList, money, productList.map(_.price).sum)
+// 洋食レストランの抽象クラス
+class WesternFoodRestaurantFactory() extends RestaurantFactory {
+  // 洋食レストランを作成するための初期処理を行っている
+  override def create(menuNum: Int): WesternFoodRestaurant = {
+    val menuSuffix = Seq("Steak", "Plate", "Set")
+    val menu = (0 to menuNum).map(i => {
+      Menu(s"WesternFood${menuSuffix(i % 3)}$i", (random() * 2000).toInt)
+    })
+    new WesternFoodRestaurant(menu)
+  }
 }
-
-// 商品を買うユーザー
-case class User(name: String, money: Int, shoppingBasket: Seq[Product])
