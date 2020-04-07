d922167b7c71fea211366984f1597e8f2f8ad8f0
yamato3310 <yamato3310.tuna@gmail.com>
2020-04-07 17:47:34 +0900

    修正

--- src/Main.scala
+++ src/Main.scala
@@ -9,159 +9,183 @@ import scala.math.pow
 */
 
 object Main extends App {
-//  val in = "(1 + (6 / 3)) * (7 - 3)"
-//  val in = "(1 + 2) + (7 - 3)"
-  val in = "3 + 6 * (6 + 5) - (7 * 2)"
+  val in = "( 1 + ( 6 / 3 )) * ( 7 - 3 )"
+//  val in = "( 1 * 2 ) * ( 3 * 4 )"
+//  val in = "3 + 6 * ( 6 + 5 ) * ( 7 * 2 )"
 //  val in = "602 % 3 * 3"
-  val nodeParser = new NodeParser()
-  val nodeSeq = nodeParser.parse(in)
-  val compiler = new Compiler()
-  val result = compiler.compile(nodeSeq)
+//  val in = "1 + 4"
+  val compiler = new Compiler(in)
+  val result = compiler.compile()
 
   println(result)
 }
 /*
-Seq[Node]を受け取り木構造に変換し、変換した木構造の計算結果を返す
+計算式を受け取り木構造に変換し、変換した木構造から計算結果を出す
  */
-class Compiler() {
+class Compiler(in: String) {
+  private val nodeParser = new NodeParser
+  val partsList: List[Parts] = nodeParser.parse(in)
   private val stack: mutable.Stack[Node] = mutable.Stack()
+  private val tempStack: mutable.Stack[IterableOnce[Node]] = mutable.Stack()
+  private val priorityQueue: mutable.Queue[Int] = mutable.Queue()
 
   /*
-  ノードのリストから木構造を作成する
-  */
-  def compile(nodes: Seq[Node], currentPriority: Int = 0): Int = nodes match {
+  PartsをNodeに変換し木構造を作成する
+  Partsはカッコを含むため、木構造ではカッコをなくすため
+  　*/
+  def compile(parts: List[Parts] = partsList, currentPriority: Int = 0): Int = parts match {
     /*
     スタックに3ノード以上ある場合は、終了せずに再帰を続ける
-     */
-    case head :: tail if tail == Nil && stack.length > 3 =>
+    　*/
+    case ::(head: Node, Nil) if stack.length > 3 =>
       val partialTree = createPartTree(head, stack.pop(), stack.pop())
-      compile(Seq(partialTree), 50)
+      compile(List(partialTree), 50)
     /*
-    最後のノードで木構造を完成させる
-    */
-    case head :: tail if tail == Nil =>
+    木構造が完成したら、計算結果を返す
+    　*/
+    case ::(head: Node, Nil) =>
       val woodStructure = createPartTree(head, stack.pop(), stack.pop())
       woodStructure.result()
     /*
-    数字のノードの場合は必ずstackさせる
+    ) が来た場合は、stack内のノードを計算しNumberにした後に、退避させていたNodeを戻す
      */
-    case head :: tail if head.priority % 100 == 10 =>
+    case ::(head: Brackets, tail) if head.value == ")"  =>
+      val partialTree = createPartTree(stack.pop(), stack.pop(), stack.pop())
+      stack.pushAll(tempStack.pop())
+      val currentPriority = priorityQueue.dequeue()
+      compile(new Number(partialTree.result().toString) +: tail, currentPriority)
+    /*
+    ( が来た場合は、stackをすべて退避させる
+     */
+    case ::(head: Brackets, tail) if head.value == "(" =>
+      tempStack.push(stack.popAll())
+      priorityQueue += currentPriority
+      compile(tail, 0)
+    /*
+    数字のPartsの場合は必ずstackさせる
+     */
+    case ::(head: Number, tail) =>
       stack.push(head)
       compile(tail, currentPriority)
     /*
     現在の優先順位より高いものが来た場合はstackする
      */
-    case head :: tail if currentPriority < head.priority =>
+    case ::(head: Sign, tail) if currentPriority < head.priority =>
       stack.push(head)
       compile(tail, head.priority)
     /*
-    初めに現在の優先順位より低いものが来た場合は、stack内の要素で部分機を作成する
-    最後に現在のstackの中身に符号が存在しない場合、次の要素をstackするために現在の優先順位を下げる
+    初めに現在の優先順位より低いものが来た場合は、stack内の要素で部分木を作成する
+    最後に現在のstackの中身にNodeが一つしか存在しない場合、次の要素をstackするために現在の優先順位を下げる
      */
-    case head :: tail if currentPriority >= head.priority =>
+    case ::(head: Sign, tail) if currentPriority >= head.priority =>
       val partialTree = createPartTree(stack.pop(), stack.pop(), stack.pop())
       stack.push(partialTree)
       val priority = if (stack.length == 1) 0 else currentPriority
       compile(head :: tail, priority)
+    case node => throw new Exception(s"no match $node")
   }
 
   /*
-  ノードと符号を受け取ることで部分機を作成する
+  ノードと符号を受け取ることで部分木を作成する
    */
   def createPartTree(left: Node, symbol: Node, right: Node): Node = {
-    new Sign(symbol.value, Some(left), Some(right), symbol.priority)
+    new Sign(symbol.value, Some(left), Some(right))
+  }
+
+  /*
+ デバック用
+  */
+  def print(): Unit = {
+    println("====")
+    partsList.foreach(node => println(node.value))
+    println("====")
   }
 }
 
-/*
-priority
-0  空白
-10 数字
-20 + -
-25 * / % ^
-30 (
-40 )
-
+/*　
 文字列で受け取った式を一つずつNodeに変換していく
  */
 class NodeParser() {
-  private var currentPriority: Int = 0
-  private var currentString: String = ""
+  private var numberDigit: String = ""
   private var spaceFlg: Boolean = true
-  private var nodeSeq: Seq[Node] = Seq()
-  private var bracketsPriority: Int = 0
-  private var bracketsEndCount: Int = 0
+  private var partsSeq: List[Parts] = List()
+  private var bracketsCount: Int = 0
+
 
   /*
-  入力された文字をNodeに変換をしていく
-  基本は空白が来るたびに、プロパティの値を見てNodeに変換をする
+  入力された文字をPartsに変換をしていく
    */
-  def parse(in: String): Seq[Node] = {
+  def parse(in: String): List[Parts] = {
     val isNum = """[0-9]""".r
     val isSign = """[\*/\+\-\^%]""".r
     val isBrackets = """[\(\)]""".r
     in.split("").foreach(s => s match {
       case isNum() => patternInt(s)
-      case isSign() => patternSign(s)
-      case isBrackets() => patternBrackets(s)
+      case isSign() => partsSeq = partsSeq :+ patternSign(s)
+      case isBrackets() => partsSeq = partsSeq :+ patternBrackets(s)
       case s if s == " " =>
         spaceFlg = true
-        nodeSeq = nodeSeq :+ createNode(currentString)
-        if (bracketsEndCount > 0) {
-          bracketsPriority -= 100 * bracketsEndCount
-          bracketsEndCount = 0
+        if (numberDigit != "") {
+          partsSeq = partsSeq :+ createNode(numberDigit)
+          numberDigit = ""
         }
-        nodeSeq
-      case s => throw new Exception(s)
+      case _ =>
     })
 
-    nodeSeq :+ createNode(currentString)
+    if (numberDigit != "") {
+      partsSeq = partsSeq :+ createNode(numberDigit)
+      numberDigit = ""
+    }
+    partsSeq
   }
 
   /*
   数字の場合、数字は2桁以上の場合ああるので、それを考慮している
    */
-  private def patternInt(s: String): Unit = s match {
-    case s if currentPriority == 10 =>
-      currentString += s
-      spaceFlg = false
-    case _ if !spaceFlg =>
+  private def patternInt(s: String): Unit = partsSeq.lastOption match {
+    case Some(_: Sign) if !spaceFlg =>
       throw new Exception("空白の位置が正しくありません")
-    case s if currentPriority != 40 =>
-      currentString = s
-      currentPriority = 10
-      spaceFlg = false
-    case _ =>
+    case Some(_: Number) =>
       throw new Exception("文法が正しくありません")
+    case _ =>
+      spaceFlg = false
+      numberDigit += s
   }
 
   /*
   記号が入力された時の処理
    */
-  private def patternSign(s: String): Unit = s match {
+  private def patternSign(s: String): Parts = partsSeq.lastOption match {
     case _ if !spaceFlg =>
-      throw new Exception("sign 空白の位置が正しくありません")
-    case s if currentPriority != 20 && currentPriority != 30  =>
-      currentString = s
-      currentPriority = 20
-      spaceFlg = false
-    case _ =>
+      throw new Exception("空白の位置が正しくありません")
+    case Some(_: Sign)  =>
       throw new Exception("文法が正しくありません")
+    case None =>
+      throw new Exception("文法が正しくありません")
+    case _ =>
+      spaceFlg = false
+      createNode(s)
+
   }
 
   /*
   カッコが入ってきた時の処理
-  Nodeに変換する際にカッコは変換せずに、カッコ内の数字と記号のpriorityを上げる処理をする
-  そのためのフラグ管理などを行っている
    */
-  private def patternBrackets(s: String): Unit = s match {
-    case s if s == "(" && (currentPriority == 20 || currentPriority == 0) =>
-      currentPriority = 30
-      bracketsPriority += 100
-    case _ if spaceFlg =>
+  private def patternBrackets(s: String): Parts = partsSeq.lastOption match {
+    case _ if !spaceFlg =>
       throw new Exception("空白の位置が正しくありません")
-    case s if s == ")" && (currentPriority == 10 || currentPriority == 40) =>
-      bracketsEndCount += 1
+    case Some(_: Number) if (s == ")" && bracketsCount > 0) =>
+      bracketsCount -= 1
+      createNode(s)
+    case Some(_: Brackets) if (s == ")" && bracketsCount > 0) =>
+      bracketsCount += 1
+      createNode(s)
+    case Some(_: Sign) if s == "(" =>
+      bracketsCount += 1
+      createNode(s)
+    case None =>
+      bracketsCount += 1
+      createNode(s)
     case _ =>
       throw new Exception("文法が正しくありません")
   }
@@ -169,51 +193,64 @@ class NodeParser() {
   /*
   受け取った文字からNodeを作成している
    */
-  private def createNode(s: String): Node = currentPriority match {
-    case i if i == 10 => new Number(s, None, None, 10 + bracketsPriority)
-    case _ if s == "+" => new Sign(s, None, None, 20 + bracketsPriority)
-    case _ if s == "-" => new Sign(s, None, None, 20 + bracketsPriority)
-    case _ if s == "/" => new Sign(s, None, None, 25 + bracketsPriority)
-    case _ if s == "*" => new Sign(s, None, None, 25 + bracketsPriority)
-    case _ if s == "%" => new Sign(s, None, None, 25 + bracketsPriority)
-    case _ if s == "^" => new Sign(s, None, None, 25 + bracketsPriority)
-    case i => throw new Exception(s"正しくない文字 ${i} ${s}")
+  private def createNode(s: String): Parts = s match {
+    case "(" => new Brackets(s)
+    case ")" => new Brackets(s)
+    case "+" => new Sign(s, None, None)
+    case "-" => new Sign(s, None, None)
+    case "/" => new Sign(s, None, None)
+    case "*" => new Sign(s, None, None)
+    case "%" => new Sign(s, None, None)
+    case "^" => new Sign(s, None, None)
+    case i => new Number(i)
   }
 
   /*
   デバック用
    */
   def print(): Unit = {
-    nodeSeq.foreach(node => println(node.value))
+    println("====")
+    partsSeq.foreach(node => println(node.value))
+    println("====")
   }
 }
 
+
 /*
 数字と記号の抽象
-leftとrightがNoneのNodeは葉ノード
-leftとrightにNodeがある場合は内部ノード
  */
-trait Node {
-  val priority: Int
-  val value: String
-  val left: Option[Node]
-  val right: Option[Node]
-
+trait Node extends Parts {
   def result(): Int
 }
 
-class Sign(val value: String, val left: Option[Node], val right: Option[Node], val priority: Int) extends Node {
+trait Parts {
+  val value: String
+}
+
+class Sign(val value: String, val left: Option[Node], val right: Option[Node]) extends Node {
+  val priority: Int = value match {
+    case "+" => 1
+    case "-" => 1
+    case "/" => 2
+    case "*" => 2
+    case "%" => 2
+    case "^" => 3
+    case _ => throw new Exception(s"存在しない文字 $value")
+  }
+
   override def result(): Int = value match {
-    case "+" => right.get.result() + left.get.result()
-    case "-" => right.get.result() - left.get.result()
-    case "/" => right.get.result() / left.get.result()
-    case "*" => right.get.result() * left.get.result()
-    case "%" => right.get.result() % left.get.result()
-    case "^" => pow(right.get.result(), left.get.result()).toInt
+    case "+" => right.fold(0)(_.result()) + left.fold(0)(_.result())
+    case "-" => right.fold(0)(_.result()) - left.fold(0)(_.result())
+    case "/" => right.fold(0)(_.result()) / left.fold(0)(_.result())
+    case "*" => right.fold(0)(_.result()) * left.fold(0)(_.result())
+    case "%" => right.fold(0)(_.result()) % left.fold(0)(_.result())
+    case "^" => pow(right.fold(0)(_.result()), left.fold(0)(_.result())).toInt
     case _ => throw new Exception(s"存在しないパターン $value")
   }
 }
 
-class Number(val value: String, val left: Option[Node], val right: Option[Node], val priority: Int) extends Node {
+class Number(val value: String) extends Node {
   override def result(): Int = value.toInt
 }
+
+class Brackets(val value: String) extends Parts
